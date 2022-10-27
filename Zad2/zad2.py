75081375259b23463b2b85e2dae483a580a5259b
Dmitry Kartashov <57914020+loneymist@users.noreply.github.com>
2022-10-27 22:48:11 +0700

    123

--- Zad2/zad2.py
+++ Zad2/zad2.py
@@ -1,46 +1,37 @@
-with open(r'Zad2\Res.txt','w') as data:
-    def isfloat(value):
-        try:
-            float(value)
-            return True
-        except ValueError:
-            return False
+check = lambda list,i,c = 0: c+1 if type(list[i]) == type(list[i-1]) else c-0
 
-    def isint(value):
-        try:
-            int(value)
-            return True
-        except ValueError:
-            return False
-    n = 0
-    i=0
-    c = 0
-    c1 = 0
-    list = []
+dict_out = {}               #задание пустого словаря
 
-    while float(n) < 3 or float(n) > 5:
-        print('How much elements do you want? (3-5)')
-        n = input()
-    while i<int(n):
-        print('Enter',i+1,'element of list')
-        list.append(input())
-        i+=1
-    print('Your list:',list)
-    data.write(r'Your list'+str(list)+'\n')
-    i = 0
-    while i < int(n):
-        if isfloat(list[i]) == True and '.' in list[i]:
-            c+=1
-        if isint(list[i]) == True:
-            c1+=1
-        i+=1
-    i = 0
-    if c == int(n):
-        print('All elemets are "Float" type')
-        data.write(r'All elemets are "Float" type')
-    if c1 == int(n):
-        print('All elements are "Int" type')
-        data.write(r'All elements are "Int" type')
-    if c1 != int(n) and c!= int(n):
-        print('Your elements are not similar type')
-        data.write(r'Your elements are not similar type')
+p = False                   #проверка на количество введенных данных
+while p == False:
+    print('--- ВВОД ДАННЫХ ---')
+    list = input('Введите данные (3-5 значений) через пробел:').split() #разделение введенных данных через пробел в список
+    if len(list)>= 3 and len(list)<=5: p = True                         #проверяем длину словаря
+    else: print('Неверное кол-во элементов.')
+prov = 0
+
+for i in range(0,len(list)):                    #проверка на типы данных, используя цикл
+    if '.' in list[i]:
+        try: 
+            list[i]=float(list[i])              #проверка на тип float, иначе str
+        except ValueError:
+            list[i] = str(list[i])
+    else: 
+        try: 
+            list[i]=int(list[i])                #проверка на тип int, иначе str
+        except ValueError:
+            list[i] = str(list[i])
+    prov += check(list,i)                       #узнаем количество одинаковых элементов, проверенных выше через лямбда функцию
+if prov == len(list)-1:                         
+    for i in range(0,len(list)):                #если все ок, обновляем наш словарь проверенными одинаковыми элементами
+        dict_out.update({i:list[i]})
+else: 
+    print('Типы различаются')
+    for i in range(0,len(list)):                #иначе обновляем наш словарь всеми типами данных, присваивая им тип str
+        dict_out.update({i:str(list[i])})
+
+print(dict_out,type(dict_out.get(0)))           #выводим словарь и его тип
+
+with open('Zad2_Out.txt','w') as data:          #записываем результат в файл вместе с типом данных
+    data.writelines(str(dict_out.items()) + '\n')
+    data.write(str(type(dict_out.get(0))))
